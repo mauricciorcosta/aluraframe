9c9c5969b5424a6e6bc47c0a223d252aa60a977b
Mauriccio Costa <mauricciorcosta@gmail.com>
2022-10-12 11:06:38 -0300

    atualizacao de codigos e criacao de arquivos

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -5,24 +5,28 @@ class NegociacaoController {
         this._inputData = $("#data");
         this._inputQuantidade = $("#quantidade");
         this._inputValor = $("#valor");
+        this._listaNegociacoes = new ListaNegociacoes();
     }
     
     adiciona(event) {
         event.preventDefault();
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._limpaFormulario();
+    }
 
-        let data = new Date(
-            ...this._inputData.value
-                .split("-")
-                .map((item,indice) => item - indice % 2)
-        );
-            
-        let negociacao = new Negociacao(
-            data,
+    _criaNegociacao() {
+        return new Negociacao(
+            DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value
         );
+    }
 
-        console.log(negociacao);
-            //add a negociacao em uma lista
+    _limpaFormulario() {
+        this._inputData.value = "";
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+
+        this._inputData.focus();
     }
 }
